2e131d0d2ef90210db7eea5704ca98f45a729101
Asya404 <unkiplum@gmail.com>
2022-02-22 12:20:02 +0100

    save courses into LS, print from LS on load

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@ const courses = document.querySelector('.courses-list'),
 
 
 
-
 // Listeners
 loadEventListeners();
 
-function loadEventListeners () {
+function loadEventListeners() {
     courses.addEventListener('click', buyCourse);
     shoppingCartContent.addEventListener('click', removeCourse);
-    clearCartBtn.addEventListener('click', clearCart)
+    clearCartBtn.addEventListener('click', clearCart);
+    document.addEventListener('DOMContentLoaded', localStorageOnLoad);
 }
 
 
@@ -24,14 +24,15 @@ function loadEventListeners () {
 // Functions
 function buyCourse(e) {
     e.preventDefault();
-    if(e.target.classList.contains('add-to-cart')) {
-        
+    if (e.target.classList.contains('add-to-cart')) {
+
         // read the course values
         const course = e.target.parentElement.parentElement.parentElement;
         getCourseInfo(course);
     }
-    
+
 }
+
 
 
 // Reads html info of the selected course
@@ -42,13 +43,13 @@ function getCourseInfo(course) {
         price: course.querySelector('.price-new').textContent,
         id: course.querySelector('.add-to-cart').getAttribute('data-id')
     }
-    console.log(courseInfo);
     addIntoCart(courseInfo);
 }
 
 
+
 // Display the selected course into the shopping cart
-function addIntoCart (course) {
+function addIntoCart(course) {
     const div = document.createElement('div');
     div.innerHTML = `
     <img src="${course.image}">
@@ -59,17 +60,66 @@ function addIntoCart (course) {
     div.classList.add('course');
 
     shoppingCartContent.appendChild(div);
+
+    saveIntoStorage(course);
 }
+
+
+
+
+// Add courses into Local Storage
+function saveIntoStorage(course) {
+    let courses = getCoursesFromStorage();
+    courses.push(course);
+    localStorage.setItem('courses', JSON.stringify(courses))
+}
+
+// Get courses from Local Storage
+function getCoursesFromStorage() {
+    let courses;
+
+    // if something exists on storage we get the value, otherwise create an empty array
+    if (localStorage.getItem('courses') === null) {
+        courses = [];
+    } else {
+        courses = JSON.parse(localStorage.getItem('courses'))
+    }
+    return courses;
+}
+
+
 
 
 // Remove course from the DOM
 function removeCourse(e) {
-    if(e.target.classList.contains('remove')) {
-       e.target.parentElement.remove();
+    if (e.target.classList.contains('remove')) {
+        e.target.parentElement.remove();
     }
 }
+
+
 
 // Clears the shopping cart
 function clearCart() {
     shoppingCartContent.innerHTML = '';
+}
+
+
+// Prints local storage courses on load
+function localStorageOnLoad() {
+    let coursesLS = getCoursesFromStorage();
+
+    // loop through the courses and print into the cart
+    coursesLS.forEach(function (course) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+    <img src="${course.image}">
+    <div>${course.title}</div>
+    <div>${course.price}</div>
+    <a href="#" class="remove" data-id="${course.id}">X</a>
+    `;
+        div.classList.add('course');
+        shoppingCartContent.appendChild(div);
+    });
+
 }
